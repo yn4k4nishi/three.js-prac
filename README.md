0696acca42741e6474815a79b1350dda0bb4e477
yn4k4ishi <yn4k4nishi.kit@gmail.com>
2022-03-12 15:09:06 +0900

    rotate cube

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # three.js practice
 
-
 https://threejs.org/manual/#en/fundamentals
+https://zenn.dev/sdkfz181tiger/books/735e854bee9fc9
--- main.js
+++ main.js
@@ -1,1 +1,49 @@
-console.log("Hello JavaScript!!");
+const W_WIDTH  = window.innerWidth; // ブラウザの横サイズ
+const W_HEIGHT = window.innerHeight;// ブラウザの縦サイズ
+const W_ASPECT = window.innerWidth / window.innerHeight;// アスペクト比
+const W_RATIO  = window.devicePixelRatio;// ピクセル比
+let camera, scene, renderer, cube;// カメラ、シーン、レンダラー、立方体
+
+window.onload = ()=>{
+	// 1, カメラを作る
+	camera = new THREE.PerspectiveCamera(50, W_ASPECT, 1, 1000);
+	camera.position.set(0, 0, 600);
+
+	// 2, シーンを作る
+	scene = new THREE.Scene();
+
+	// 3-1, ライトを作る1
+	let dirLight = new THREE.DirectionalLight(0xffffff, 1);
+	dirLight.position.set(5,3,5);// 光の向き
+	scene.add(dirLight);
+	
+    // 3-2, ライトを作る2
+	let ambLight = new THREE.AmbientLight(0x333333);
+	scene.add(ambLight);
+	
+    // 4, レンダラーを作る
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setPixelRatio(W_RATIO);// ピクセル比
+	renderer.setSize(W_WIDTH, W_HEIGHT);
+	
+    // 5, HTMLに配置する
+	let div = document.getElementById("three");
+	div.appendChild(renderer.domElement);
+	
+    // 6, キューブを配置する
+	let geometry = new THREE.BoxGeometry(200, 200, 200);
+	let material = new THREE.MeshLambertMaterial({color: 0x00ddff});
+	cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
+	
+    // 7, アニメーションの開始
+	animate();
+}
+
+function animate(){
+	cube.rotation.x += 0.002;// 立方体を回転
+	cube.rotation.y += 0.002;
+	// cube.rotation.z += 0.002;
+	renderer.render(scene, camera);// レンダリング
+	requestAnimationFrame(animate);// 更新
+}
